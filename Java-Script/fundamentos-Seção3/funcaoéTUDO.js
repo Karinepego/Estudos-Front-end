7d3b14cb3a2641f1e3adbcb5060a5178703deeff
Karinepego <karinepegoribeiro@gmail.com>
2020-06-25 23:11:08 -0300

    Meus estudos em Front-end

---
+++ Java-Script/fundamentos-Seção3/funcaoéTUDO.js
@@ -0,0 +1,5 @@
+console.log(typeof Object,' yes! is a fuction!')
+
+class produto {} //qualque classe q vc crie é uma função interna
+console.log(typeof Produto) 
+
--- Java-Script/fundamentos-Seção3/nullUnderfined.js
+++ Java-Script/fundamentos-Seção3/nullUnderfined.js
@@ -1,3 +1,30 @@
+let valor // nao inicializada undefined
+console.log(valor)
+
+let valor2 = null       
+console.log(valor2)
+//console.log(valor.toString()) erro! vc nunca vai conseguir acessar um let com valor nulo
+
+console.log('-----------')
+
+const produto = {}
+    console.log(produto.preco)
+    console.log(produto)
+
+console.log('-----------')
+
+produto.preco = undefined //evite atribuir undefined deixe a linguagem definir o q é undefinded e o q n é
+    console.log ('produto.preco undefined em boolean: >>>',!!produto.preco)
+// delete produto.preco
+    console.log(produto, 'produto undefined')
+
+console.log('-----------')
+
+produto.preco = null //sem preço esta gratuito 
+console.log('produto.preco null em boolean:>>>',!!produto.preco)
+console.log(produto, 'produto gratuito')
+
+
 /*atribuição por referencia 
 apontam para o mesmo endereço da mameoria 1
 caixote com 2 variaveis apontando para ela,
@@ -9,5 +36,4 @@ a variaval c serve de refrencia/ispiração para variavel d,
 mas elas nao possuem o mesmo endereço de memoria,
 caso um mude o seu valor isso n afeta a outra 
  */
-
 
