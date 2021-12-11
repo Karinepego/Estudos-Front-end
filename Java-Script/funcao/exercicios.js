c13b9bb3f6bef2b65dcc875343af9a7bb2bc49e6
Karinepego <karinepegoribeiro@gmail.com>
2021-12-10 23:44:12 -0300

    continuação

--- Java-Script/funcao/exercicios.js
+++ Java-Script/funcao/exercicios.js
@@ -142,15 +142,33 @@ console.log(divisivel(18))
 */
 
 /*
-08) Pedro joga N jogos de basquete por temporada. Para saber como está ele está progredindo, ele mantém
-registro de todos os as pontuações feitas por jogo. Após cada jogo ele anota no novo valor e confere se o
-mesmo é maior ou menor que seu melhor e pior desempenho. Dada uma lista string = “pontuação1 pontuação2
-pontuação3 etc..”, escreva uma função que ao recebê-la irá comparar os valores um a um e irá retornar um
-vetor com o número de vezes que ele bateu seu recorde de maior número de pontos e quando fez seu pior
-jogo. (Número do pior jogo).
-Obs.: O primeiro jogo não conta como novo recorde do melhor.
-Exemplo:
-String: “10 20 20 8 25 3 0 30 1”
-Retorno: [3, 7] (Significa que ele bateu três vezes seu recorde de melhor pontuação e a pior pontuação
-aconteceu no sétimo jogo.)
+09) Construa uma função para um sistema de notas de uma instituição que possui a seguinte política de
+classificação: Todo aluno recebe uma nota de 0 a 100. Alunos com nota abaixo de 40 são reprovados. As notas
+possuem a seguinte regra de arredondamento: Se a diferença entre a nota e o próximo múltiplo de 5 for menor
+que 3, arredondar a nota para esse próximo múltiplo de 5. Se a nota for abaixo de 38, não é feito nenhum
+arredondamento pois esta nota resulta na reprovação do aluno. Por exemplo, a nota 84 será arredondada para
+85, mas a nota 29 não será arredondada por ser abaixo de 40 e não ser possível arredondamento eficiente, ou
+seja, que evite a reprovação do aluno. No caso de a nota ser 38, o arredondamento é possível pois atingirá 40
+e o aluno será aprovado.
  */ 
+
+function classificaoAluno(notaPrincipal){
+    let notaArredondada
+    if(notaPrincipal >= 40){ 
+        return console.log("voce foi aprovado")
+    }else{
+
+        return console.log("voce foi reprovado")
+    }
+}
+
+function calculaNota(notaPrincipal){
+    if (notaPrincipal % 5 > 2) { //multiplos de 5 = divididos por 5, resultam em uma divisão exata e sem resto
+        return notaPrincipal + (5 - (notaPrincipal % 5))
+    } else {
+        return notaPrincipal
+    }
+}
+
+classificaAluno(100)
+classificaAluno(38)
