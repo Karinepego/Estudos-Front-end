df339e49ce3de7e9aabe795eaa789213252c88b0
Karinepego <karinepegoribeiro@gmail.com>
2022-01-16 22:03:51 -0300

    continuação

--- Java-Script/funcao/exercicios.js
+++ Java-Script/funcao/exercicios.js
@@ -260,3 +260,283 @@ switch(fruta){
         return console.log("opção invalida tente novamente ")
 }
 */
+/* Um homem decidiu ir à uma revenda comprar um carro. Ele deseja comprar um carro hatch, e a revenda
+possui, além de carros hatch, sedans, motocicletas e caminhonetes. Utilizando uma estrutura switch, caso o
+comprador queira o hatch, retorne: “Compra efetuada com sucesso”. Nas outras opções, retorne: “Tem certeza
+que não prefere este modelo?”. Caso seja especificado um modelo que não está disponível, retorne no console:
+“Não trabalhamos com este tipo de automóvel aqui”.
+ 
+function escolherAutomovel(automovel){
+    switch(automovel){
+        case "hatch":
+            return console.log('Compra efetuada com sucesso');
+            break;
+        case "sedans": 
+        case "motocicletas":
+        case "caminhonetes":
+            return console.log("Tem certeza que não prefere este modelo?: hatch");
+            break;
+        default :
+            return console.log("Não trabalhamos com este tipo de automóvel aqui")
+    }S
+}
+
+escolherAutomovel("hatch"); 
+escolherAutomovel("sedans"); 
+escolherAutomovel("motocicletas"); 
+escolherAutomovel("caminhonetes"); 
+escolherAutomovel("qualquer um"); 
+*/
+
+/*16) Utilizando a estrutura do Switch faça um programa que simule uma calculadora básica O programa recebe
+como parâmetros dois valores numéricos e uma string referente à operação e a realize com os valores
+numéricos na ordem que foram inseridos. Por exemplo: calculadora (2, ‘+’, 3). A função efetuará a soma de 2 e
+3. Dica: Os sinais das operações são: ‘+’. ‘-’, ‘*’ e ‘/’. Crie um caso default para operações inválidas.  
+
+function calculadora(valor1, operador, valor2){
+    switch(operador){
+        case "+":
+            return console.log(`${valor1} + ${valor2} = ${valor1 + valor2}` );
+            break;
+        case "-": 
+            return console.log(`${valor1} - ${valor2} = ${valor1 - valor2}` );
+            break;
+        case "*":
+            return console.log(`${valor1} * ${valor2} = ${valor1 * valor2}` );
+            break;
+        case "/":
+            return console.log(`${valor1} / ${valor2} = ${valor1 / valor2}`)
+            break
+        default :
+            return console.log("operções inválidas")
+    }
+}
+
+calculadora(2,"+",2); 
+calculadora(2,"-",2); 
+calculadora(2,"*",2); 
+calculadora(2,"/",2); 
+calculadora(2,"ç",2);
+*/
+
+/*17) Um funcionário irá receber um aumento de acordo com o seu plano detrabalho, de acordo com a tabela abaixo: 
+Plano| Aumento
+A    |   10%
+B    |   15%
+C    |   20%
+Faça uma função que leia o plano de trabalho e o salário atual de um funcionário e calcula e imprime o seu
+novo salário. Use a estrutura switch e faça um caso default que indique que o plano é inválido. 
+
+function mudaPlano(plano, saAtual){
+    switch(plano){
+        case "A":
+            return console.log(`plano A = Salario atual: ${saAtual}; Novo salario: ${(saAtual/10)+saAtual}` );
+            break;
+        case "B": 
+            return console.log(`Plano B = Salario atual: ${saAtual}; Novo salario: ${saAtual * 1.15}` );
+            break;
+        case "C":
+            return console.log(`Salario atual: ${saAtual}; Novo salario: ${saAtual * 1.2}` );
+            break
+        default :
+            return console.log("operções inválidas")
+    }
+}
+
+mudaPlano("A", 1000); 
+mudaPlano("B", 1000); 
+mudaPlano("C", 1000); 
+mudaPlano("d", 1000); */
+
+/*18) Faça um programa que leia um número entre 0 e 10, e escreva este número por extenso. Use o comando
+switch. Crie um case default que escreva ‘Número fora do intervalo.’ 
+function numeroPorExtenso(numero) {
+    switch (numero) {
+        case 0:
+            console.log('Zero')
+            break
+        case 1:
+            console.log('Um')
+            break
+        case 2:
+            console.log('Dois')
+            break
+        case 3:
+            console.log('Três')
+            break
+        case 4:
+            console.log('Quatro')
+            break
+        case 5:
+            console.log('Cinco')
+            break
+        case 6:
+            console.log('Seis')
+            break
+        case 7:
+            console.log('Sete')
+            break
+        case 8:
+            console.log('Oito')
+            break
+        case 9:
+            console.log('Nove')
+            break
+        case 10:
+            console.log('Dez')
+            break
+        default:
+            console.log('Numero fora do intervalo.')
+    }
+}
+
+numeroPorExtenso(0)
+numeroPorExtenso(1)
+numeroPorExtenso(2)
+numeroPorExtenso(3)
+numeroPorExtenso(4)
+numeroPorExtenso(5)
+numeroPorExtenso(6)
+numeroPorExtenso(7)
+numeroPorExtenso(8)
+numeroPorExtenso(9)
+numeroPorExtenso(10)
+numeroPorExtenso(11) */
+
+/*19) O cardápio de uma lanchonete é o seguinte:
+Implemente uma função que receba como parâmetros o código do item pedido, a quantidade e calcule o valor
+a ser pago por aquele lanche. Considere que a cada execução somente será calculado um item. Use o
+comando switch. Crie um caso default para produto não existente.
+ 
+function lanchonete(cod, quan){
+    switch (cod) {
+        case 100:
+            return console.log("cao quente, " +quan+ " unidades = " + 3.00*quan)
+        case 200:
+            return qtd * 4
+        case 300:
+            return qtd * 5.5
+        case 400:
+            return qtd * 7.5
+        case 500:
+            return qtd * 3.5
+        case 600:
+            return qtd * 2.8
+        default:
+            return 'Produto não existente.'
+    }
+
+}
+
+lanchonete(100,1);
+lanchonete(200, 2);
+lanchonete(300, 2);
+lanchonete(400, 2);
+lanchonete(500, 2);
+lanchonete(600, 2);
+lanchonete(700, 2);
+*/
+
+/*24) Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
+
+let count = 0;
+ while (count <= 11 ) {
+	console.log(count + " Hello world")
+
+	count++;
+} 
+*/
+
+/*25) Escrever um programa para exibir os números de 1 até 50 na tela 
+let count = 1;
+ while (count <= 50 ) {
+	console.log(count )
+	count++;
+} 
+
+function contador () {
+    for (let i = 1; i<=50; i++) {
+        console.log(i);
+    }
+}
+contador();
+*/
+
+/*26) Fazer um programa para encontrar todos os pares entre 1 e 100. 
+
+function contador () {
+    for (let i = 1; i<=50; i++) {
+        if(i % 2 == 0){
+            console.log(i + " é par!")
+        }
+    }
+}
+contador();
+*/
+
+/*20) Crie um programa para informar quais e quantas notas são necessárias para entregar o mínimo de cédulas
+para um determinado valor informado pelo usuário considerando notas de R$ 100, R$ 50, R$ 10 e R$ 5 e R$ 1.
+Seu programa deve mostrar apenas as notas utilizadas. Por exemplo, ao solicitar R$18, o programa deve
+informar apenas a seguinte informação (note que não foram exibidas informações sobre as demais cédulas):
+ 1 nota(s) de R$ 10. 
+ 1 nota(s) de R$ 5. 
+ 3 nota(s) de R$ 1. 
+
+function dinheiroContado(valor){
+ cont = valor - 10;
+ seila = cont - 5;
+
+}
+
+dinheiroContado(18)*/
+
+/* 28) Ler um vetor de números inteiros e imprimir quantos são pares e quantos são ímpares.
+
+function quantidadePares(vetor){
+let impares = 0;
+let pares = 0;
+    for(let i = 0; i < vetor.length; i++ ){
+        if (vetor[i] %2 == 0){
+            pares ++; 
+        }else{
+            impares ++;
+            
+        }
+    }
+    console.log(`Existem ${pares} números pares e ${impares} números ímpares.`)
+}
+
+vetor = [1,2,3,4,5,6,7,8,9,10];
+quantidadePares(vetor)*/
+
+/*21) Criar um programa para identificar o valor a ser pago por um plano de saúde dada a idade do conveniado
+considerando que todos pagam R$ 100 mais um adicional conforme a seguinte tabela: 
+1) crianças com menos de 10 anos pagam R$80; 
+2) conveniados com idade entre 10 e 30 anos pagam R$50; 
+3) conveniados com idade acima de 30 e até 60 anos pagam R$ 95; 
+4) conveniados acima de 60 anos pagam R$130 
+
+function PlanoSaude(idade){
+        if(idade <= 10){
+            return console.log(`Voce ira pagar 100$ convenio e 80% por mês`);
+        }else if((idade > 10) & (idade < 30)){
+            return console.log(`Voce ira pagar 100$ convenio e 50% por mês`);
+        }else if( idade >= 30 & idade < 60){
+            return console.log(`Voce ira pagar 100$ convenio e 95% por mês`);
+        }else{
+            return console.log(`Voce ira pagar 100$ convenio e 130% por mês`);  
+        }
+}
+PlanoSaude(9)
+PlanoSaude(11)
+PlanoSaude(31)
+PlanoSaude(61)*/
+
+/* 29) Utilizando a estrutura de repetição for, faça uma função que percorra um vetor e conte quantos números
+deste vetor estão no intervalo [10,20] (repare que o intervalo é fechado, ou seja, inclui o 10 e o 20) e quantos
+deles estão fora do intervalo, escrevendo estas informações. */
+
+
+
+
+
